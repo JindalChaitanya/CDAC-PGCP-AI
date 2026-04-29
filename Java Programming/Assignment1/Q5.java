f1f070c29eb0ecca7a655da84c18441d17eff8c4
UsarNaam <chaitanya.jindal2002@gmail.com>
2026-04-29 11:56:27 +0530

    Completed Assignment1.Q5

--- Java Programming/Assignment1/Q5.java
+++ Java Programming/Assignment1/Q5.java
@@ -1,16 +1,42 @@
 // JAVA Programming Assignment - 1
 // Submitted by Chaitanya Jindal
 
-// Q5. Check whether a given year is a leap year.
-// Hint if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-// A student’s marks are entered. Display the grade based on the marks.
+// Q5. A student’s marks are entered. Display the grade based on the marks.
 // • 90 and above → A
 // • 75–89 → B
 // • 50–74 → C
 // • Below 50 → Fail
 
+package Assignment1;
+
+import java.util.Scanner;
+
 public class Q5 {
+
+    int inputMarks(Scanner sc) {
+        System.out.print("Enter the marks: ");
+        return sc.nextInt();
+    }
+
+    String getGrade(int marks) {
+        if (marks >= 90) {
+            return "A";
+        } else if (marks >= 75) {
+            return "B";
+        } else if (marks >= 50) {
+            return "C";
+        } else {
+            return "Fail";
+        }
+    }
+
     public static void main(String[] args) {
-        // Write your code here
+        
+        Q5 obj = new Q5();
+        Scanner sc = new Scanner(System.in);
+        int marks = obj.inputMarks(sc);
+        String grade = obj.getGrade(marks);
+        System.out.println("Grade: " + grade);
+        sc.close();
     }
 }
