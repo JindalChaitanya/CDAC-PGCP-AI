21a0dd09e391a6fe83504bb86cd78fd11422fc32
UsarNaam <chaitanya.jindal2002@gmail.com>
2026-04-29 12:19:41 +0530

    Completed Assignment1.Q6

--- Java Programming/Assignment1/Q6.java
+++ Java Programming/Assignment1/Q6.java
@@ -7,8 +7,27 @@
 // • Above 300 units → ₹10 per unit
 // Write a Java program to calculate the total bill based on units consumed.
 
+package Assignment1;
+
+import java.util.Scanner;
+
 public class Q6 {
+    double calculateBill(int units) {
+        double bill = 0;
+        if (units <= 100)
+            bill = units * 5;
+        else if (units <= 300)
+            bill = 100 * 5 + (units - 100) * 7;
+        else
+            bill = 100 * 5 + 200 * 7 + (units - 300) * 10;
+        return bill;
+    }
     public static void main(String[] args) {
-        // Write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the units consumed: ");
+        int units = sc.nextInt();
+        double bill = new Q6().calculateBill(units);
+        System.out.println("Total bill: " + bill);
+        sc.close();
     }
 }
