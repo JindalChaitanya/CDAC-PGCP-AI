cc3e4f54b6c9b75fe115a7bfcc97413d1384b90e
UsarNaam <chaitanya.jindal2002@gmail.com>
2026-04-29 12:21:45 +0530

    Updated

--- Java Programming/Assignment1/Q7.java
+++ Java Programming/Assignment1/Q7.java
@@ -7,11 +7,6 @@
 // Write a program to check login credentials and display:
 // • “Login Successful”
 // • “Invalid Username or Password”
-// Q8. A store gives discount based on purchase amount:
-// • Above ₹5000 → 20% discount
-// • ₹2000–₹5000 → 10% discount
-// • Below ₹2000 → No discount
-// Write a program to calculate the final payable amount.
 
 public class Q7 {
     public static void main(String[] args) {
---
+++ Java Programming/Assignment1/Q8.java
@@ -0,0 +1,13 @@
+// JAVA Programming Assignment - 1
+// Submitted by Chaitanya Jindal
+// Q8. A store gives discount based on purchase amount:
+// • Above ₹5000 → 20% discount
+// • ₹2000–₹5000 → 10% discount
+// • Below ₹2000 → No discount
+// Write a program to calculate the final payable amount.
+
+public class Q8 {
+    public static void main(String[] args) {
+        // Write your code here
+    }
+}
