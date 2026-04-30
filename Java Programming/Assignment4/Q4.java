7c7cb0f2df6c37c717625d81151934d4117b44d7
UsarNaam <chaitanya.jindal2002@gmail.com>
2026-04-30 12:22:04 +0530

    Completed Assignment4.Q4

--- Java Programming/Assignment4/Q4.java
+++ Java Programming/Assignment4/Q4.java
@@ -14,14 +14,51 @@
 // • Apply discount if total > 5000
 
 package Assignment4;
-
 import java.util.Scanner;
+
+class InvalidQuantityException extends Exception {
+    public InvalidQuantityException(String message) {
+        super(message);
+    }
+}
+
+class InvalidPriceException extends Exception {
+    public InvalidPriceException(String message) {
+        super(message);
+    }
+}
 
 public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Write your code here
+        try {
+            System.out.print("Enter product price: ");
+            double price = sc.nextDouble();
+            System.out.print("Enter quantity: ");
+            int quantity = sc.nextInt();
+
+            if (price <= 0) {
+                throw new InvalidPriceException("InvalidPriceException");
+            }
+            if (quantity <= 0) {
+                throw new InvalidQuantityException("InvalidQuantityException");
+            }
+
+            double total = price * quantity;
+            if (total > 5000) {
+                total *= 0.9;
+            }
+            System.out.println("Total bill: " + total);
+
+
+        } catch (InvalidPriceException e) {
+            System.out.println(e.getMessage());
+        } catch (InvalidQuantityException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Invalid Input: " + e.getMessage());
+        }
 
         sc.close();
     }
