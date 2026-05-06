869a44236131a21537f76118fb13a6657ac362ab
JindalChaitanya <jindalchaitanya@icloud.com>
2026-05-06 17:58:17 +0530

    Updated

---
+++ Java Programming/Assignment 5/CMS.java
@@ -0,0 +1,41 @@
+// JAVA Programming Assignment - 5
+// (Submitted by Chaitanya Jindal)
+
+// Q1. Create a small application in Java for College Management System.
+
+// Requirements
+// • Create a class Teacher containing:
+//   • Name
+//   • Qualification
+
+// • Extend the Teacher class into a Department class containing:
+//   • Department Number
+//   • Department Name
+
+// • Create an interface named College containing:
+//   • One field: Name of the College
+//   • Two abstract methods:
+//     • getDetails()
+//     • showDetails()
+//   • One default method displaying a general message
+//   • One static method displaying a general message
+
+// • Override base class and interface methods in the derived class
+
+// • Ask department name from the user and validate:
+//   • "IT dept"
+//   • "Management dept"
+//
+// • If department name does not match, display:
+//   • "Not a valid Record"
+
+// • Assign department numbers as:
+//   • IT dept → 10
+//   • Management dept → 20
+
+// • Display appropriate information using the created classes and interface
+// • Create a class CMS containing the main() method where all functionalities are called
+
+public class CMS {
+    
+}
